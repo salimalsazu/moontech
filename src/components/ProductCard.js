8d2b4fcc60a0ac3cbc7adf5163aca92b50166e53
Salim Al Sazu <accsalimalsazu@gmail.com>
2023-02-06 14:29:04 +0600

    push

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
+import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { addToCart, removeFromCart } from "../redux/actionCreators/productAction";
+
 
 const ProductCard = ({ product }) => {
+
+  const { pathname } = useLocation();
+  const dispatch = useDispatch();
+
+
   return (
     <div
-      className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'
+      className='shadow-lg relative rounded-3xl border  p-3 flex flex-col text-indigo-900'
       key={product._id}
     >
+      {
+        pathname.includes('cart') && <div className="bg-indigo-500 text-white p-1 absolute rounded">
+          <p>{product.quantity}</p>
+        </div>
+      }
       <div className='h-52 w-52 mx-auto'>
         <img src={product.image} alt={product.model} />
       </div>
@@ -20,15 +34,24 @@ const ProductCard = ({ product }) => {
         </ul>
       </div>
       <div className='flex gap-2 mt-5'>
-        <button className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
-          Add to cart
-        </button>
-        <button
-          title='Add to wishlist'
-          className='bg-indigo-500  py-1 px-2 rounded-full'
-        >
-          <BiListPlus className='text-white' />
-        </button>
+        {
+          !pathname.includes("cart") && <button onClick={() => dispatch(addToCart(product))} className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
+            Add to cart
+          </button>
+        }
+        {
+          pathname.includes("cart") && <button onClick={() => dispatch(removeFromCart(product))} className='bg-red-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
+            Romove From Cart
+          </button>
+        }
+        {
+          !pathname.includes("cart") && <button
+            title='Add to wishlist'
+            className='bg-indigo-500  py-1 px-2 rounded-full'
+          >
+            <BiListPlus className='text-white' />
+          </button>
+        }
       </div>
     </div>
   );
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import ProductCard from "../components/ProductCard";
 
 const Cart = () => {
+
+  const cart = useSelector((state) => state.cart)
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is cart</h1>
+      {cart.sort((a, b) => a._id - b._id).map(product => <ProductCard product={product}></ProductCard>)}
     </div>
   );
 };
